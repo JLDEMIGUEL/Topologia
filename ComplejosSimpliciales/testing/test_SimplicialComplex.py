7874c5586c9f10b219efc967cadc2b4c09389135
Jose Luis De Miguel <joseluis.demiguel@vipera.com>
2022-11-27 10:41:39 +0100

    Added VietorisRips Examples and VietorisRips tests.

--- ComplejosSimpliciales/testing/test_SimplicialComplex.py
+++ ComplejosSimpliciales/testing/test_SimplicialComplex.py
@@ -1,10 +1,12 @@
+import json
+import os
+from pathlib import Path
 from unittest import TestCase
-
 import numpy as np
-
 from ComplejosSimpliciales.src.AlphaComplex import AlphaComplex
 from ComplejosSimpliciales.src.SimplicialComplex import SimplicialComplex
 from ComplejosSimpliciales.src.utils.constants import tetraedro, toro, plano_proyectivo, botella_klein
+CLOUD_PATH = os.path.join(os.path.abspath(Path(__file__).parent.parent.parent),"docs","clouds.json")
 
 
 class TestSimplicialComplex(TestCase):
@@ -16,11 +18,7 @@ class TestSimplicialComplex(TestCase):
     sc3 = SimplicialComplex([(0,), (1,), (2, 3), (4, 5), (5, 6), (4, 6), (6, 7, 8, 9)])
 
     # AlphaComplex
-    ac4 = AlphaComplex(np.array([(0.38021546727456423, 0.46419202339598786), (0.7951628297672293, 0.49263630135869474),
-                                 (0.566623772375203, 0.038325621649018426), (0.3369306814864865, 0.7103735061134965),
-                                 (0.08272837815822842, 0.2263273314352896), (0.5180166301873989, 0.6271769943824689),
-                                 (0.33691411899985035, 0.8402045183219995), (0.33244488399729255, 0.4524636520475205),
-                                 (0.11778991601260325, 0.6657734204021165), (0.9384303415747769, 0.2313873874340855)]))
+    ac4 = AlphaComplex(np.array(json.load(open(CLOUD_PATH))['alpha1']))
 
     # Plano Proyectivo
     plano_proyectivo = plano_proyectivo
